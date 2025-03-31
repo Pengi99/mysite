f7d29c4e90f1f0af91fde2fad40c6e941c001c9b
Pengi99 <whwhdgh99@naver.com>
2025-03-31 10:45:47 +0900

    test update

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def invest():
 
 @app.route('/dashboard')
 def dashboard():
+    input_code = request.args['code']
+    input_year = request.args['month']
+    input_month = request.args['year']
+    input_day = request.args['day']
+    input_time = f'{input_year}-{input_month}-{input_day}'
+    input_type = request.args['type']
+
     return render_template('dashboard.html')
 
 app.run(debug=True)
