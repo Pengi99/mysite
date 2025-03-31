9678b1e7618f395fbf32ce29d85d0f4c54635fe5
Pengi99 <whwhdgh99@naver.com>
2025-03-31 11:20:44 +0900

    ready table

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, url_for
 import pandas as pd
+import invest
 
 
 app = Flask(__name__)
@@ -18,6 +19,27 @@ def dashboard():
     input_time = f'{input_year}-{input_month}-{input_day}'
     input_type = request.args['type']
 
-    return render_template('dashboard.html')
+    df = invest.load_data(input_code, input_time)
+    invest_class = invest.Invest(df, _col='Close', _start=input_time)
+    if input_type == 'bnh':
+        result = invest_class.buyandhold()
+    elif input_type == 'boll':
+        result = invest_class.bollinger()
+    elif input_type == 'mmt':
+        result = invest_class.momentum()
+    result.reset_index(inplace=True)
+    result = result[['Date', 'Close', 'trade', 'rtn', 'acc_rtn']]
+    result.columns = ['시간', '종가', '보유내역', '일별 수익율', '누적 수익율']
+    cols_list = list(result.columns)
+    dict_data = result.to_dict(orient='records')
+    x_data = list(result['시간'])
+    y_data = list(result['일별 수익율'])
+    y1_data = list(result['누적 수익율'])
+    return render_template('dashboard.html',
+                           table_cols=cols_list,
+                           table_data=dict_data,
+                           x_data=x_data,
+                           y_data=y_data,
+                           y1_data=y1_data)
 
 app.run(debug=True)
