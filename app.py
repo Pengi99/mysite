696515d166d4194db63fad31cda069ee58da644a
Pengi99 <whwhdgh99@naver.com>
2025-03-31 13:47:31 +0900

    update

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import invest
 app = Flask(__name__)
 
 @app.route('/invest')
-def invest():
+def first():
     return render_template('invest.html')
 
 
 @app.route('/dashboard')
 def dashboard():
     input_code = request.args['code']
-    input_year = request.args['month']
-    input_month = request.args['year']
+    input_year = request.args['year']
+    input_month = request.args['month']
     input_day = request.args['day']
     input_time = f'{input_year}-{input_month}-{input_day}'
     input_type = request.args['type']
@@ -27,9 +27,17 @@ def dashboard():
         result = invest_class.bollinger()
     elif input_type == 'mmt':
         result = invest_class.momentum()
+    result = result[['Close', 'trade', 'rtn', 'acc_rtn']]
+    result['ym'] = result.index.strftime('%Y-%m')
+    result = pd.concat(
+        [
+            result.groupby('ym')[['Close', 'trade', 'acc_rtn']].max(),
+            result.groupby('ym')[['rtn']].mean()
+        ], axis=1
+    )
     result.reset_index(inplace=True)
-    result = result[['Date', 'Close', 'trade', 'rtn', 'acc_rtn']]
-    result.columns = ['시간', '종가', '보유내역', '일별 수익율', '누적 수익율']
+    result.columns = ['시간', '종가', '보유내역', '누적 수익율', '일별 수익율']
+
     cols_list = list(result.columns)
     dict_data = result.to_dict(orient='records')
     x_data = list(result['시간'])
